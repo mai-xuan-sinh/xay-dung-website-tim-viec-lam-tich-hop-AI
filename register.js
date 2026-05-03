4ed00963d3c89f102dfb2b0f949a187a4ce0edd8
maixuanbeo <maixuansinh27@gmail.com>
2026-05-03 15:52:10 +0700

    sua dang nhap voi dang ki giao dien

--- register.js
+++ register.js
@@ -1,40 +1,48 @@
+// TYPE SWITCH
 const typeCards = document.querySelectorAll('.type-card');
-const userTypeInput = document.getElementById('userType');
 const employerFields = document.getElementById('employerFields');
 
 typeCards.forEach(card => {
-  card.addEventListener('click', () => {
-
-    // đổi active
+  card.onclick = () => {
     typeCards.forEach(c => c.classList.remove('active'));
     card.classList.add('active');
 
-    const value = card.getAttribute('data-value');
-    userTypeInput.value = value;
-
-    // 👉 SHOW / HIDE field công ty
-    if (value === "employer") {
+    if (card.dataset.value === "employer") {
       employerFields.style.display = "block";
     } else {
       employerFields.style.display = "none";
     }
-  });
+  };
 });
+
+// SHOW PASSWORD
+document.querySelectorAll('.toggle-pass').forEach(icon => {
+  icon.onclick = () => {
+    const input = icon.previousElementSibling;
+    input.type = input.type === "password" ? "text" : "password";
+    icon.classList.toggle("fa-eye-slash");
+  };
+});
+
+
 
 
 const modal = document.getElementById("policyModal");
 const openBtn = document.getElementById("openPolicy");
 const closeBtn = document.querySelector(".close");
 
+// mở
 openBtn.onclick = (e) => {
   e.preventDefault();
   modal.classList.add("show");
 };
 
+// đóng bằng X
 closeBtn.onclick = () => {
   modal.classList.remove("show");
 };
 
+// click ra ngoài thì đóng
 window.onclick = (e) => {
   if (e.target === modal) {
     modal.classList.remove("show");
